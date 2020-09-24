3222d4c77838612a87268cefc61c8fe91e753abb
andrew <a89852637185@gmail.com>
2020-09-24 15:55:35 +0300

    realese all

--- app.js
+++ app.js
@@ -20,6 +20,29 @@ app.get('/constructor', (req, res) => {
   res.render('constructor')
 })
 
+app.post('/constructor', (req, res) => {
+  res.send()
+})
+
+app.get('/basket', (req, res) => {
+  res.render('basket')
+})
+
+app.post('/basket', (req, res) => {
+  res.redirect('/basket')
+})
+
+app.get('/liked', (req, res) => {
+  res.render('liked')
+})
+
+app.post('/liked', (req, res) => {
+  res.redirect('/liked')
+})
+
+app.post('/catalog', (req, res) => {
+  res.redirect('/catalog')
+})
 
 app.listen(3000);
 
